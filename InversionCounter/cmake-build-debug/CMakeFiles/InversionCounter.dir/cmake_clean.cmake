625019c2c0b4f10ac3de217474fb212e330aa09b
Semal31 <35205813+Semal31@users.noreply.github.com>
2020-12-14 01:26:25 -0500

    IC

---
+++ InversionCounter/cmake-build-debug/CMakeFiles/InversionCounter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InversionCounter.dir/inversioncounter.cpp.obj"
+  "InversionCounter.exe"
+  "InversionCounter.exe.manifest"
+  "InversionCounter.pdb"
+  "libInversionCounter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InversionCounter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
